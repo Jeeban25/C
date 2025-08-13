33f80d3297597f72cdf32c127b1e3298390501dd
Jeeban Bohara <jeebanbohara98@gmail.com>
2025-08-14 00:42:08 +0545

    printing_string_character.c

--- printing_string_character.c
+++ printing_string_character.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    
+    char ch='a';
+    char str[20]="I Love C Programming";
+    printf("Case 1 value of ch is: %c\n", ch);
+    printf("Case 2 value of ch is: %10c\n", ch);
+    printf("Case 3 value of str is: %s\n", str);
+    printf("Case 4 value of str is: %18s\n", str);
+    printf("Case 5 value of str is: %-15.8s\n", str);
+    return 0;
 }
