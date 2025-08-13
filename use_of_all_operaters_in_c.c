45b66cbde28ad88ddcd4d1cc82e59cd77bfc38ae
Jeeban Bohara <jeebanbohara98@gmail.com>
2025-08-13 21:05:13 +0545

    Use of All Operators in C

---
+++ use_of_all_operaters_in_c.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    int a=8, b=2, c=5;
+    float d=9.2, e=10.2;
+    printf("a+b = %d\n", a+b); // Addition
+    printf("a-b = %d\n", a-b); // Subtraction
+    printf("a/b = %d\n", a/b); // Division
+    printf("a*b = %d\n", a*b); // Multiplication
+    printf("a%%b = %d\n", a%b); // Modulus
+    printf("a==b = %d\n", a==b); // Equal to
+    printf("a!=b = %d\n", a!=b); // Not equal to
+    printf("a>b = %d\n", a>b); // Greater than 
+    printf("a<b = %d\n", a<b); // Less than
+    printf("a>=b = %d\n", a>=b); // Greater than or equal to
+    printf("a<=b = %d\n", a<=b); // Less than or equal to
+    printf("d+e = %.2f\n", d+e); // Float Addition  
+    printf("d-e = %.2f\n", d-e); // Float Subtraction
+    printf("d/e = %.2f\n", d/e); // Float Division
+    printf("d*e = %.2f\n", d*e); // Float Multiplication
+    printf("d==e = %.2f\n", d==e); // Float Equal
+    printf("d!=e = %.2f\n", d!=e); // Float Not equal
+    printf("d>e = %.2f\n", d>e); // Float Greater
+    printf("d<e = %.2f\n", d<e); // Float Less
+    printf("d>=e = %.2f\n", d>=e); // Float Greater than or equal to
+    printf("d<=e = %.2f\n", d<=e); // Float Less
+    printf("a&&b = %d\n", a&&b); // Logical AND
+    printf("a||b = %d\n", a||b); // Logical OR 
+    printf("!a = %d\n", !a); // Logical NOT
+    printf("a&b = %d\n", a&b); // Bitwise AND
+    printf("a|b = %d\n", a|b); // Bitwise OR
+    printf("a^b = %d\n", a^b); // Bitwise XOR
+    printf("~a = %d\n", ~a); // Bitwise NOT
+    printf("a<<1 = %d\n", a<<1); // Left Shift
+    printf("a>>1 = %d\n", a>>1); // Right Shift
+    printf("sizeof(a) = %u\n", sizeof(a)); // Size of operator
+    printf("sizeof(d) = %u\n", sizeof(d)); // Size of float operator
+    printf("sizeof(int) = %u\n", sizeof(int)); // Size of int type
+    printf("sizeof(float) = %u\n", sizeof(float)); // Size of float type
+    printf("sizeof(char) = %u\n", sizeof(char)); // Size of char type
+    printf("sizeof(double) = %u\n", sizeof(double)); // Size of double type
+    printf("sizeof(long) = %u\n", sizeof(long)); // Size of long type
+    printf("sizeof(long long) = %u\n", sizeof(long long)); // Size of long long type
+    printf("sizeof(short) = %u\n", sizeof(short)); // Size of short type
+    printf("sizeof(long double) = %u\n", sizeof(long double)); // Size of long double type
+    printf("a++ = %d\n", a++); // Post-increment
+    printf("++b = %d\n", ++b); // Pre-increment
+    printf("a-- = %d\n", a--); // Post-decrement
+    printf("--c = %d\n", --c); // Pre-decrement
+    printf("a+=b = %d\n", a+=b); // Addition assignment
+    printf("a-=b = %d\n", a-=b); // Subtraction assignment
+    printf("a*=b = %d\n", a*=b); // Multiplication assignment
+    printf("a/=b = %d\n", a/=b); // Division assignment
+    printf("a%%=b = %d\n", a%=b); // Modulus
+    printf("a&=b = %d\n", a&=b); // Bitwise AND assignment
+    printf("a|=b = %d\n", a|=b); // Bitwise OR assignment
+    printf("a^=b = %d\n", a^=b); // Bitwise XOR assignment
+    printf("a<<=1 = %d\n", a<<=1); // Left Shift assignment
+    printf("a>>=1 = %d\n", a>>=1); // Right Shift assignment
+    printf("d+=e = %.2f\n", d+=e); // Float Addition    
+    printf("d-=e = %.2f\n", d-=e); // Float Subtraction
+    printf("d*=e = %.2f\n", d*=e); // Float Multiplication
+    printf("d/=e = %.2f\n", d/=e); // Float Division    
+    printf("d==e = %.2f\n", d==e); // Float Equal
+    printf("d!=e = %.2f\n", d!=e); // Float Not equal
+    printf("d>e = %.2f\n", d>e); // Float Greater   
+    printf("d<e = %.2f\n", d<e); // Float Less
+    printf("d>=e = %.2f\n", d>=e); // Float Greater than or equal to
+    printf("d<=e = %.2f\n", d<=e); // Float Less than or equal to
+}
